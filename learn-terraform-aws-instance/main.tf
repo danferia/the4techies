db6e8d121086d646188f68c99384a7a166db90e9
danferia <daniela.feria.795@my.csun.edu>
2022-03-11 22:21:02 -0800

    main edit vpc, subnets

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -10,113 +10,28 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  profile = "default"
+  region  = "us-east-2"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
+resource "aws_s3_bucket" "s3bucket"{
+  bucket = "the4techies-20220309"
+  acl    = "private"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.64.0"
-
-  cidr = var.vpc_cidr_block
-
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, 2)
-  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, 2)
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+resource "aws_vpc" "nginix-vpc"{
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
 }
 
-module "app_security_group" {
-  source  = "terraform-aws-modules/security-group/aws//modules/web"
-  version = "3.17.0"
-
-  name        = "web-server-sg"
-  description = "Security group for web-servers with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
-}
-
-module "lb_security_group" {
-  source  = "terraform-aws-modules/security-group/aws//modules/web"
-  version = "3.17.0"
-
-  name        = "lb-sg-project-alpha-dev"
-  description = "Security group for load balancer with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "random_string" "lb_id" {
-  length  = 3
-  special = false
-}
-
-module "elb_http" {
-  source  = "terraform-aws-modules/elb/aws"
-  version = "2.4.0"
-
-  # Ensure load balancer name is unique
-  name = "lb-${random_string.lb_id.result}-project-alpha-dev"
-
-  internal = false
-
-  security_groups = [module.lb_security_group.this_security_group_id]
-  subnets         = module.vpc.public_subnets
-
-  number_of_instances = length(module.ec2_instances.instance_ids)
-  instances           = module.ec2_instances.instance_ids
-
-  listener = [{
-    instance_port     = "80"
-    instance_protocol = "HTTP"
-    lb_port           = "80"
-    lb_protocol       = "HTTP"
-  }]
-
-  health_check = {
-    target              = "HTTP:80/index.html"
-    interval            = 10
-    healthy_threshold   = 3
-    unhealthy_threshold = 10
-    timeout             = 5
-  }
-}
-
-module "ec2_instances" {
-  source = "./modules/aws-instance"
-
-  instance_count     = var.instances_per_subnet * length(module.vpc.private_subnets)
-  instance_type      = var.instance_type
-  subnet_ids         = module.vpc.private_subnets[*]
-  security_group_ids = [module.app_security_group.this_security_group_id]
-}
-
-resource "aws_db_subnet_group" "private" {
-  subnet_ids = module.vpc.private_subnets
-}
-
-# Warning: The following is only an example. Never check sensitive values like
-# usernames and passwords into source control.
-
-resource "aws_db_instance" "database" {
-  allocated_storage = 5
-  engine            = "mysql"
-  instance_class    = "db.t2.micro"
-  username          = "admin"
-  password          = "notasecurepassword"
-
-  db_subnet_group_name = aws_db_subnet_group.private.name
-
-  skip_final_snapshot = true
+resource "aws_subnet" "pro-subnet-public" {
+  vpc_id                  = aws_vpc.nginix-vpc.id 
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-2"
 }
 
 resource "aws_instance" "app_server" {
@@ -124,7 +39,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "AppServerInstance"
     Name = var.instance_name
   }
 }
