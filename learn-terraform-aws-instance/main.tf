afe3ba502c6314bd253cdc2c87247aefd5b19362
danferia <daniela.feria.795@my.csun.edu>
2022-03-26 02:35:59 +0000

    user data main.tf variabels and edit nginx

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -13,14 +13,89 @@ provider "aws" {
   profile = "default"
   region  = "us-east-2"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-resource "aws_s3_bucket" "s3bucket"{
-  bucket = "the4techies-20220309"
-  acl    = "private"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+
 }
 
-resource "aws_vpc" "nginx-vpc"{
-  cidr_block           = "10.0.0.0/16"
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  key_name        = var.key
+  user_data       = <<-EOT
+#cloud-config
+#
+# This is an example file to automatically configure resolv.conf when the
+# instance boots for the first time.
+#
+# Ensure that your yaml is valid and pass this as user-data when starting
+# the instance. Also be sure that your cloud.cfg file includes this
+# configuration module in the appropriate section.
+#
+#Downloads the apache and golang
+packages:
+ - apache2
+ - golang-go
+ - git
+runcmd:
+ - mkdir /home/ubuntu/go
+ - export GOPATH=/home/ubuntu/go
+ - go get github.com/hashicorp/lear-go-webapp-demo
+EOT
+  security_groups = [aws_security_group.allow_tls.name]
+
+  tags = {
+    Name = "${var.env}"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-dd1c28b8"
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "s22-cit481"
+  }
+}
+
+resource "aws_vpc" "nginx-vpc" {
+  cidr_block           = "10.0.0.0/24"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   enable_classiclink   = "false"
@@ -28,23 +103,21 @@ resource "aws_vpc" "nginx-vpc"{
 }
 
 resource "aws_subnet" "pro-subnet-public" {
-  vpc_id                  = aws_vpc.nginx-vpc.id 
-  route {
-    cidr_block              = "10.0.1.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone       = "us-east-2"
-  }
-  tags ={
+  vpc_id                  = aws_vpc.nginx-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-2"
+  tags = {
     Name = "prod-public-crt"
   }
 }
 
-resource "aws_route_table_association" "prod-crta-public-subnet-1"{
-  subnet_id      = aws_subnet.pro-subnet-public-1.id
+resource "aws_route_table_association" "prod-crta-public-subnet" {
+  subnet_id      = aws_subnet.pro-subnet-public.id
   route_table_id = aws_route_table.prod-public-crt.vpc_id
 }
 
-resource "aws_security_group" "ssh-allowed" {
+/*resource "aws_security_group" "ssh-allowed" {
   vpc_id = aws_vpc.nginx-vpc.id
   egress {
     from_port   = 0
@@ -52,7 +125,7 @@ resource "aws_security_group" "ssh-allowed" {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -66,19 +139,12 @@ resource "aws_security_group" "ssh-allowed" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
+}*/
 
 resource "aws_key_pair" "aws-key" {
-  key_name = "aws-key"
-  public_key = file(var.public_key_path) 
+  key_name   = "aws-key"
+  public_key = file(var.public_key_path)
 }
-
-resource "aws_instance" "nginx_server" {
-  acl    = "public"
-}
-
-resource "aws_default_vpc" "default"{}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
@@ -87,33 +153,23 @@ resource "aws_instance" "app_server" {
     Name = "nginx_server"
     Name = var.instance_name
   }
-#VPC
-  subnet_id = aws_subnet.pro-subnet-public-1.id
+  #VPC
+  subnet_id = aws_subnet.pro-subnet-public.id
 
-#Security Group
+  #Security Group
   vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
 
-#The Public SSH Key
+  #The Public SSH Key
 
   key_name = aws_key_pair.aws-key.id
 
-#Nginix Install
-  provisioner "file" {
-   source      = "nginx.sh"
-   destination = "/tmp/nginx.sh"
+  #Nginx 
+  #Setting up the ssh connection to install nginx server
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("${var.private_key_path}")
   }
-  provisioner "remote-exec" {
-   inline = [
-     "sudo amazon-linux-extras install -y nginx",
-     "sudo systemctl start nginx"
-   ]
-  }
-#Setting up the ssh connection to install nginx server
- connection {
-  type        = "ssh"
-  host        = self.public_ip
-  user        = "ubuntu"
-  private_key = file("${var.private_key_path}")
- }
 
 }
