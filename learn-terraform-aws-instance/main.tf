cf8f77963c5ae0c48f6cd935f4c4ebbe3c3459d4
danferia <daniela.feria.795@my.csun.edu>
2022-04-16 13:07:36 -0700

    Nginx syntax fix/ main .tf working minus instnace deletion

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  /*region      = "us-east-2b"
-  access_key  = "my_access_key"
+  region  = "us-east-2"
+  /*access_key  = "my_access_key"
   secret_key = "my_secret_key"*/
 }
 
@@ -96,6 +96,49 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0c7478fd229861c57" # us-east-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+
+
+/*
 resource "aws_vpc" "nginx-vpc" {
   cidr_block           = "10.0.0.0/24"
   enable_dns_support   = "true"
@@ -103,7 +146,7 @@ resource "aws_vpc" "nginx-vpc" {
   enable_classiclink   = "false"
   instance_tenancy     = "default"
 }
-/*
+
 resource "aws_subnet" "pro-subnet-public" {
   vpc_id                  = aws_vpc.nginx-vpc.id
   cidr_block              = "10.0.1.0/24"
