47d4f91155ab1421ca1691973c254b444640e53f
danferia <daniela.feria.795@my.csun.edu>
2022-05-12 21:33:36 -0700

    VPC, Subnets/ Hard coded, S3, acm,Route 53

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 3.27"
     }
   }
+ backend "s3" {
+    bucket = "the4techs-terraform"
+    key = "prod/terraform.tfstate"
+    region = "us-east-2"
+  }
 
   required_version = ">= 0.14.9"
 }
@@ -14,6 +19,10 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+provider "aws" {
+  alias = "acm_provider"
+  region = "us-east-1"
+}
 
 #userdata AWS Resource AWS_Instance
 data "aws_ami" "ubuntu" {
@@ -22,7 +31,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220420"]
   }
 
   filter {
@@ -59,14 +68,14 @@ EOT
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = "vpc-0931500aaf97153e1"
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [data.aws_vpc.selected.cidr_block]
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
   }
 
   ingress {
@@ -74,7 +83,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [data.aws_vpc.selected.cidr_block]
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
   }
 
   ingress {
@@ -82,7 +91,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [data.aws_vpc.selected.cidr_block]
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
   }
 
   egress {
@@ -90,7 +99,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -98,7 +106,248 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 #VPC user Data
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.31.0.0/16"
 
-data "aws_vpc" "selected" {
-  id = var.vpc_id
+  tags = {
+    Name = "tf_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "vpc-0931500aaf97153e1"
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "tf-subnet"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = "subnet-0525f9c9bd7a7632c"
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0aeb7c931a5a61206" # us-east-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+###################################################################################################
+#Route 53
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+  tags = var.common_tags
+}
+
+resource "aws_route53_record" "root-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = var.domain_name
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = "www.${var.domain_name}"
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+########################################################################################
+##CloudFront
+
+# Cloudfront distribution for main s3 site.
+resource "aws_cloudfront_distribution" "www_s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.www_bucket.website_endpoint
+    origin_id = "S3-www.${var.bucket_name}"
+
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+
+  aliases = ["www.${var.domain_name}"]
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code = 404
+    response_code = 200
+    response_page_path = "/404.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-www.${var.bucket_name}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 31536000
+    default_ttl = 31536000
+    max_ttl = 31536000
+    compress = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  tags = var.common_tags
+}
+
+# Cloudfront S3 for redirect to www.
+resource "aws_cloudfront_distribution" "root_s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.root_bucket.website_endpoint
+    origin_id = "S3-.${var.bucket_name}"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+
+  aliases = [var.domain_name]
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-.${var.bucket_name}"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin"]
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl = 0
+    default_ttl = 86400
+    max_ttl = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+  tags = var.common_tags
+}
+# SSL Certificate
+resource "aws_acm_certificate" "ssl_certificate" {
+  provider = aws.acm_provider
+  domain_name = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method = "EMAIL"
+  #validation_method = "DNS"
+
+  tags = var.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider = aws.acm_provider
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+#########################################################################################################
+
+#s3
+# S3 bucket for website.
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = "www.${var.bucket_name}"
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = var.common_tags
+}
+
+# S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = var.bucket_name
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+
+  website {
+    redirect_all_requests_to = "https://www.${var.domain_name}"
+  }
+
+  tags = var.common_tags
 }
